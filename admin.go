f9fd9a0c92c0fad82ecfd1cb42311a84ac9c7574
sathvamgroups-cell <sathvamgroups@gmail.com>
2026-09-16 22:59:56 +0530

    Create admin.go

---
+++ admin.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		role, exists := c.Get("role")
+
+		if !exists {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "Role not found",
+			})
+			c.Abort()
+			return
+		}
+
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "Admin access required",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
